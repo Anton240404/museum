2a5b9ce2a0a0cf5f29fb909038b6bd11f0851809
Anton24044 <anton.agafonov.97@bk.ru>
2025-06-09 16:24:51 +0300

    добавлены роуты

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-
-import './App.css'
+import styles from './reset.module.css';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 function App() {
 
-  return (
-    <>
+    return (
+        <div className={styles.reset}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/hero/:id" element={<Hero />} />
+                    <Route path="/search" element={<Search />} />
+                </Routes>
+            </BrowserRouter>
 
-    </>
-  )
+        </div>
+    );
 }
 
-export default App
+export default App;
